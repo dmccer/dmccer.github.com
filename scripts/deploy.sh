0355293955edc14f9fd60405029455ffcaf88dc7
xiaoyh0926 <xiaoyh0926@tpl.cntaiping.com>
2019-07-30 10:27:10 +0800

    个人页面样式调整

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,17 +2,23 @@
 
 set -euvo pipefail
 
+PROJ=personal-site
 OUT_DIR=${PWD}/deploy
 mkdir -p $OUT_DIR
 
-yarn build
+DEST_DIR=./dist
+mkdir -p $DEST_DIR
+rm -rf $DEST_DIR/*
 
-TAR_NAME=$(date +%Y-%m-%d)
-tar -zcvf $OUT_DIR/$TAR_NAME.tar.gz ./dist
+cp ./index.html $DEST_DIR
+cp -r ./res $DEST_DIR
+
+TAR_NAME=personal-$(date +%Y-%m-%d)
+tar -zcvf $OUT_DIR/$TAR_NAME.tar.gz $DEST_DIR
 
 OPS_USER=root
 IP=106.14.202.241
 REMOTE_DIR=/home/admin/www
 
 scp $OUT_DIR/$TAR_NAME.tar.gz $OPS_USER@$IP:$REMOTE_DIR/tars
-ssh $OPS_USER@$IP /home/admin/deploy.sh $TAR_NAME $REMOTE_DIR
+ssh $OPS_USER@$IP /home/admin/deploy.sh $TAR_NAME $REMOTE_DIR $PROJ
